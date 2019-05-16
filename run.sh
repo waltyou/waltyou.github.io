dc4fc6e2edc51cbc810ed310a68d73dcb7295cde
waltyou <waltyou@163.com>
2019-05-16 21:00:23 +0800

    add new branch

--- run.sh
+++ run.sh
@@ -1,1 +1,9 @@
+# sudo apt-get install ruby ruby-dev build-essential
+# echo '# Install Ruby Gems to ~/gems' >> ~/.bashrc
+# echo 'export GEM_HOME=$HOME/gems' >> ~/.bashrc
+# echo 'export PATH=$HOME/gems/bin:$PATH' >> ~/.bashrc
+# source ~/.bashrc
+# gem install jekyll bundler
+# bundle install
+
 bundle exec jekyll serve
